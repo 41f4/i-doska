c8fe9bb0d377715351f2c6109924d4e0cdde810d
Aleksey <kutepoff@gmail.com>
2017-03-26 14:53:49 +0300

    Initial commit

---
+++ docs/js/googleMapsHandler.js
@@ -0,0 +1,50 @@
+let map;
+
+function initMap(options) {
+    options = options || {
+            zoom: 13,
+            center: {
+                lat: 56.328938,
+                lng: 43.962387
+            }
+        };
+    let mapElement = document.getElementById('map-placeholder');
+    map = new google.maps.Map(mapElement, options);
+}
+
+function errorCallback(err) {
+    console.warn("Error getting current postion: ${err.code} - ${err.message}");
+    initMap();
+}
+
+function successCallback(pos) {
+    initMap({
+        center: {
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude
+        },
+        zoom: 8
+    });
+}
+
+function onGoogleMapsReady() {
+    if (navigator.geolocation) {
+        let geoOptions = {
+            maximumAge: 15000,
+            timeout: 2,
+            enableHighAccuracy: true
+
+        };
+        try {
+            navigator.geolocation.getCurrentPosition(
+                successCallback, errorCallback, geoOptions);
+        }
+        catch(e) {
+            errorCallback({code: 'N/A', message: e.message})
+        }
+
+    } else {
+        errorCallback({code: 'N/A', message: "Browser doesn't support Geolocation"})
+    }
+}
+
